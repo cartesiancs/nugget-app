3cdff8aaf6643b536ce40d4450016ee0b992e085
Dipokal HHJ <hhj@devent.kr>
2023-01-15 18:01:54 +0900

    fix: merge the file uploader into a component
    + 드래그 앤 드롭 파일 업로더를 컴포넌트로 만들기

---
+++ src/components/asset/assetUploader.js
@@ -0,0 +1,80 @@
+class AssetDropUploader extends HTMLElement { 
+    constructor() {
+        super();
+
+    }
+
+    render(){
+        const template = this.template();
+
+        this.classList.add("bg-dark", "position-absolute", "d-none")
+        this.style.left = '0px'
+        this.style.top = '0px'
+        this.style.width = '100%'
+        this.style.height = '100%'
+        this.style.zIndex = "10000"
+        this.style.opacity = '80%'
+        this.style.paddingTop = '46vh'
+
+        this.innerHTML = template;
+    }
+
+    template() {
+        return `
+        <div class="row justify-content-center align-items-start">
+            <b class="col-12 align-self-center text-light position-fixed text-center">
+                여기에 파일을 드롭해주세요
+            </b>
+        </div>`
+    }
+
+
+
+
+    handleDragEnter() {
+        console.log('dragenter');
+        this.classList.remove("d-none")
+    }
+
+    handleDragOver(e) {
+        e.preventDefault();
+        console.log('dragover');
+
+        this.classList.remove("d-none")
+    }
+
+    handleDragLeave() {
+        this.classList.add("d-none")
+    }
+
+    handleDrop(e) {
+        e.preventDefault();
+
+        console.log('drop');
+
+        let filePath = e.dataTransfer.files[0].path
+        console.log(filePath)
+        NUGGET.asset.add(filePath)
+        this.classList.add("d-none")
+    }
+
+
+
+    connectedCallback() {
+        this.render();
+
+        document.addEventListener('dragenter', this.handleDragEnter.bind(this))
+        document.addEventListener('dragover', this.handleDragOver.bind(this))
+        this.addEventListener('dragleave', this.handleDragLeave.bind(this))
+        this.addEventListener('drop', this.handleDrop.bind(this))
+
+    }
+
+    disconnectedCallback(){
+
+    }
+
+}
+
+
+export { AssetDropUploader }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import renderAnimation from './functions/renderAnimation.js';
 
 import { AssetList, AssetFile, AssetFolder } from './components/asset/assetList.js';
 import { AssetBrowser } from './components/asset/assetBrowser.js';
+import { AssetDropUploader } from './components/asset/assetUploader.js';
+
+
 import { ElementTimeline, ElementTimelineCursor, ElementTimelineRuler } from './components/element/elementTimeline.js';
 import { ElementBar } from './components/element/elementBar.js';
 import { ElementControl, ElementControlAsset } from './components/element/elementControl.js';
@@ -25,6 +28,9 @@ customElements.define('asset-list', AssetList);
 customElements.define('asset-file', AssetFile);
 customElements.define('asset-folder', AssetFolder);
 customElements.define('asset-browser', AssetBrowser);
+customElements.define('asset-upload-drop', AssetDropUploader);
+
+
 
 
 customElements.define('element-timeline', ElementTimeline);

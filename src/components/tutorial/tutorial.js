fb21e4a7807cd6d28a9f3e63884b6a32b79976d4
Dipokal HHJ <hhj@devent.kr>
2023-03-25 14:17:32 +0900

    feat: update version
    + 버전 업데이트

--- src/components/tutorial/tutorial.js
+++ src/components/tutorial/tutorial.js
@@ -8,9 +8,39 @@ class Tutorial extends HTMLElement {
     render() {
         //this.hideAllOptions()
 
+        this.innerHTML = this.template()
+
+        this.tutorialModal = new bootstrap.Modal(document.querySelector("div[ref='tutorial']"), {
+            keyboard: false
+        })
+
+        //this.tutorialModal.show()
+
     }
 
 
+    template() {
+        return `<div class="modal fade show" ref="tutorial" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content bg-default">
+                <div class="modal-body text-center text-light bg-default">
+                    <h5 class="modal-title font-weight-lg">너겟 튜토리얼</h5>
+                    <p class="font-weight-md"></p>
+                    <div class="d-flex bd-highlight">
+                    <div class="me-auto p-2 bd-highlight"><button type="button" class="d-flex align-items-center btn btn-sm btn-secondary"><span class="material-symbols-outlined">
+                    arrow_back
+                    </span></button></div>
+
+                    <div class="p-2 bd-highlight"><button type="button" class="d-flex align-items-center btn btn-sm btn-primary"><span class="material-symbols-outlined">
+                    navigate_next
+                    </span></button></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>`
+    }
 
     connectedCallback() {
         this.render();

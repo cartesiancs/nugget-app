c480902f6631b91312e6cff405d6d864d72e063f
DipokalLab <hhj@devent.kr>
2023-05-02 21:10:01 +0900

    fix: load extension
    + 파일로 된 익스텐션 파일 불러오기 수정

--- electron/lib/extension.ts
+++ electron/lib/extension.ts
@@ -1,5 +1,6 @@
 
 import { window } from "./window.js"
+import { app } from 'electron'
 
 import path from 'path'
 import fs from 'fs'
@@ -10,7 +11,7 @@ import isDev from 'electron-is-dev'
 import DecompressZip from 'decompress-zip';
 
 const manifestFilename = "manifest.json"
-const resourcesPath = isDev == true ? '/Users/hhj/Documents/NUGGET_EXT_FOLD' : process.resourcesPath
+const resourcesPath = isDev == true ? app.getPath("temp") : app.getPath("temp")
 
 
 type manifestType = {
@@ -56,6 +57,7 @@ class Extension {
             const filename = await this.getFilenameFromPath({ dir: this.file })
             this.directory = path.join(`${resourcesPath}/${filename}`)
         }
+        console.log("this.directory", this.directory)
 
 
         const isExistManifest = await this.isExistManifest()
@@ -64,6 +66,8 @@ class Extension {
         }
 
         this.manifest = await this.getManifest()
+
+
         await this.loadWindow({ index: this.manifest.index })
     }
 
@@ -89,26 +93,34 @@ class Extension {
     }
 
     async unzip() {
+        const filename = await this.getFilenameFromPath({ dir: this.file })
         const unzipper = new DecompressZip(this.file)
 
-        unzipper.on('error', function (err) {
-            console.log('Caught an error');
-        });
-        
-        unzipper.on('extract', function (log) {
-            console.log('Finished extracting');
-        });
-        
-        unzipper.on('progress', function (fileIndex, fileCount) {
-            console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
-        });
-        
-        unzipper.extract({
-            path: path.join(`${resourcesPath}/`),
-            filter: function (file) {
-                return file.type !== "SymbolicLink";
-            }
-        });
+        const isDone = new Promise((resolve, reject) => {
+            unzipper.on('error', function (err) {
+                console.log('Caught an error');
+                reject(false)
+            });
+            
+            unzipper.on('extract', function (log) {
+                resolve(true)
+            });
+            
+            unzipper.on('progress', function (fileIndex, fileCount) {
+                console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
+            });
+            
+            unzipper.extract({
+                path: path.join(`${resourcesPath}/${filename}`),
+                filter: function (file) {
+                    return file.type !== "SymbolicLink";
+                }
+            });
+        })
+
+        return isDone
+
+
     }
 
     async loadManifestFromDir() {

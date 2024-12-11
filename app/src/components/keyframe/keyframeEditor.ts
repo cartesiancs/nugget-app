580e43b04a4ebe381df01ebca514b0059d9dcede
Huh Hyeongjun <hhj@devent.kr>
2024-12-11 20:51:03 +0900

    fix: draw keyframe

--- app/src/components/keyframe/keyframeEditor.ts
+++ app/src/components/keyframe/keyframeEditor.ts
@@ -5,7 +5,7 @@ import {
   ITimelineStore,
   useTimelineStore,
 } from "../../states/timelineStore";
-import { millisecondsToPx } from "../../utils/time";
+import { millisecondsToPx, pxToMilliseconds } from "../../utils/time";
 import { uiStore } from "../../states/uiStore";
 
 @customElement("keyframe-editor")
@@ -81,6 +81,17 @@ export class KeyframeEditor extends LitElement {
       this.drawCanvas();
     });
 
+    this.lineCount =
+      this.timeline[this.elementId].animation[this.animationType].points.length;
+
+    this.clearLineEditorGroup();
+
+    for (let line = 0; line < this.lineCount; line++) {
+      this.addLineEditor(line);
+    }
+
+    this.changeLineEditor(0);
+
     return this;
   }
 
@@ -90,9 +101,6 @@ export class KeyframeEditor extends LitElement {
     // this.divBody = this.querySelector("div");
     // this.svgBody = this.divBody.querySelector("svg");
     // this.keyframePointBody = this.divBody.querySelector("keyframe-point");
-
-    // this.lineCount =
-    //   this.timeline[this.elementId].animation[this.animationType].points.length;
 
     // if (
     //   this.timeline[this.elementId].animation[this.animationType].isActivate ==
@@ -110,14 +118,6 @@ export class KeyframeEditor extends LitElement {
     //     this.timeline[this.elementId].animation[
     //       this.animationType
     //     ].points[1][0][1];
-    // }
-
-    // this.clearLineEditorGroup();
-
-    // for (let line = 0; line < this.lineCount; line++) {
-    //   this.addLineEditor(line);
-    //   this.drawLine(line, true);
-    //   this.loadPoint(line);
     // }
 
     // const timelineRange = Number(
@@ -173,21 +173,24 @@ export class KeyframeEditor extends LitElement {
 
     for (let index = 0; index < pointsX.length; index++) {
       const element = pointsX[index];
-      ctx.fillStyle = "#dbdaf0";
+      ctx.fillStyle = "#403af0";
 
       ctx.beginPath();
+
       const x =
         millisecondsToPx(element[0], this.timelineRange) - this.timelineScroll;
-      ctx.arc(element[0], element[1], 4, 0, 2 * Math.PI);
+      ctx.arc(x, element[1], 4, 0, 2 * Math.PI);
       ctx.fill();
     }
 
     for (let index = 0; index < pointsY.length; index++) {
       const element = pointsY[index];
-      ctx.fillStyle = "#dbdaf0";
+      ctx.fillStyle = "#ed3c21";
 
       ctx.beginPath();
-      ctx.arc(element[0], element[1], 4, 0, 2 * Math.PI);
+      const x =
+        millisecondsToPx(element[0], this.timelineRange) - this.timelineScroll;
+      ctx.arc(x, element[1], 4, 0, 2 * Math.PI);
       ctx.fill();
     }
   }
@@ -196,15 +199,30 @@ export class KeyframeEditor extends LitElement {
     const pointsX = this.timeline[this.elementId].animation.position.points[0];
     const pointsY = this.timeline[this.elementId].animation.position.points[1];
 
-    ctx.strokeStyle = "#dbdaf0";
+    ctx.strokeStyle = "#403af0";
 
     ctx.beginPath();
     ctx.moveTo(0, 0);
 
     for (let index = 0; index < pointsX.length; index++) {
       const element = pointsX[index];
+      const x =
+        millisecondsToPx(element[0], this.timelineRange) - this.timelineScroll;
+      ctx.lineTo(x, element[1]);
+    }
 
-      ctx.lineTo(element[0], element[1]);
+    ctx.stroke();
+
+    ctx.strokeStyle = "#ed3c21";
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+
+    for (let index = 0; index < pointsY.length; index++) {
+      const element = pointsY[index];
+      const x =
+        millisecondsToPx(element[0], this.timelineRange) - this.timelineScroll;
+      ctx.lineTo(x, element[1]);
     }
 
     ctx.stroke();
@@ -286,7 +304,10 @@ export class KeyframeEditor extends LitElement {
   }
 
   _handleMouseClick(e) {
-    const insertX = e.offsetX;
+    const insertX =
+      pxToMilliseconds(e.offsetX, this.timelineRange) +
+      pxToMilliseconds(this.timelineScroll, this.timelineRange);
+
     const insertY = e.offsetY;
 
     this.addPoint({
@@ -370,32 +391,6 @@ export class KeyframeEditor extends LitElement {
     document.querySelector("#timelineOptionLineEditor").innerHTML = "";
   }
 
-  loadPoint(line) {
-    if (
-      this.timeline[this.elementId].animation[this.animationType].isActivate ==
-      true
-    ) {
-      let points =
-        this.timeline[this.elementId].animation[this.animationType].points[
-          line
-        ];
-
-      for (let index = 0; index < points.length; index++) {
-        const element = points[index];
-        if (element[0] == 0) {
-          continue;
-        }
-        this.addPoint({
-          x: element[0],
-          y: element[1],
-          line: line,
-        });
-      }
-
-      this.drawLine(line, true);
-    }
-  }
-
   addPoint({ x, y, line }) {
     this.insertPointInMiddle({
       x: Math.round(x),
@@ -411,7 +406,7 @@ export class KeyframeEditor extends LitElement {
 
     let loadPointLength =
       this.points[line][this.points[line].length - 1][0] - 1;
-    //let allPoints = this.getInterpolatedPoints(loadPointLength, line);
+    //let allPoints = this.interpolateBezier(this.points[line]);
 
     this.timeline[this.elementId].animation[this.animationType].isActivate =
       true;
@@ -420,21 +415,6 @@ export class KeyframeEditor extends LitElement {
     // this.timeline[this.elementId].animation[this.animationType].allpoints[
     //   line
     // ] = allPoints;
-  }
-
-  drawPoint({ x, y, line }) {
-    const timelineRange = Number(
-      document.querySelector("element-timeline-range").value,
-    );
-    const timeMagnification = timelineRange / 4;
-
-    let insertY = y - 4;
-    let insertX = (x - 4) * timeMagnification;
-
-    this.keyframePointBody.insertAdjacentHTML(
-      "beforeend",
-      `<div class="position-absolute keyframe-point" style="top: ${insertY}px; left: ${insertX}px;"></div>`,
-    );
   }
 
   insertPointInMiddle({ x, y, line }) {
@@ -465,134 +445,6 @@ export class KeyframeEditor extends LitElement {
       }
     });
     return tmp;
-  }
-
-  drawLine(line, isinit = true) {
-    this.querySelector("svg").insertAdjacentHTML(
-      "beforeend",
-      `
-        <polyline id="keyframePolyline${line}" />
-        <path id="keyframePath${line}" class="keyframe-path-${line + 1}" />
-        <path id="keyframeHiddenPath${line}" class="d-none" />`,
-    );
-
-    const timelineRange = Number(
-      document.querySelector("element-timeline-range").value,
-    );
-    const timeMagnification = timelineRange / 4;
-
-    let points: any = [];
-
-    for (let index = 0; index < this.points[line].length; index++) {
-      points.push([
-        this.points[line][index][0] * timeMagnification,
-        this.points[line][index][1],
-      ]);
-    }
-
-    this.path[line] = this.svgBody.querySelector(
-      `path[id='keyframePath${line}']`,
-    );
-    this.path[line].setAttribute("d", this.drawPath(points, this.tension));
-
-    this.hiddenPath[line] = this.svgBody.querySelector(
-      `path[id='keyframeHiddenPath${line}']`,
-    );
-    this.hiddenPath[line].setAttribute(
-      "d",
-      this.drawPath(this.points[line], this.tension),
-    );
-
-    let loadPointLength =
-      this.points[line][this.points[line].length - 1][0] - 1;
-    let allPoints = this.getInterpolatedPoints(loadPointLength, line);
-    this.timeline[this.elementId].animation[this.animationType].allpoints[
-      line
-    ] = allPoints;
-  }
-
-  drawPath(points, tension) {
-    if (tension == null) tension = 1;
-    let size = points.length * 2;
-    let last = size - 4;
-    let path = "M" + [points[0][0], points[0][1]];
-    let now = 0;
-
-    for (let i = 0; i < size - 2; i += 2) {
-      let x0 = now ? points[now - 1][0] : points[0][0];
-      let y0 = now ? points[now - 1][1] : points[0][1];
-      let x1 = points[now][0];
-      let y1 = points[now][1];
-      let x2 = points[now + 1][0];
-      let y2 = points[now + 1][1];
-      let x3 = i !== last ? points[now + 2][0] : x2;
-      let y3 = i !== last ? points[now + 2][1] : y2;
-      let cp1x = x1 + ((x2 - x0) / 6) * tension;
-      let cp1y = y1 + ((y2 - y0) / 6) * tension;
-      let cp2x = x2 - ((x3 - x1) / 6) * tension;
-      let cp2y = y2 - ((y3 - y1) / 6) * tension;
-      now += 1;
-      path += "C" + [cp1x, cp1y, cp2x, cp2y, x2, y2];
-    }
-    return path;
-  }
-
-  getPointAt(x, line) {
-    let from = 0;
-    let to = this.hiddenPath[line].getTotalLength();
-    let current = (from + to) / 2;
-    let point = this.hiddenPath[line].getPointAtLength(current);
-
-    while (Math.abs(point.x - x) > 0.5) {
-      if (point.x < x) from = current;
-      else to = current;
-      current = (from + to) / 2;
-      point = this.hiddenPath[line].getPointAtLength(current);
-    }
-
-    return {
-      x: point.x,
-      y: point.y,
-    };
-  }
-
-  getInterpolatedPoints(loadPointLength, line) {
-    let points = [];
-    let indexDivision = 4;
-    let indexAt = 0;
-
-    for (
-      let index = 0;
-      index < Math.round(loadPointLength / indexDivision);
-      index++
-    ) {
-      points.push(this.getPointAt(indexAt, line));
-      indexAt += 4;
-    }
-
-    return points;
-  }
-
-  handleMousedown(e) {
-    const timelineRange = Number(
-      document.querySelector("element-timeline-range").value,
-    );
-    const timeMagnification = timelineRange / 4;
-
-    let insertX = e.offsetX / timeMagnification;
-    let insertY = e.offsetY;
-    this.addPoint({
-      x: insertX,
-      y: insertY,
-      line: this.selectLine,
-    });
-
-    this.drawLine(this.selectLine, true);
-
-    let animationPanel = document.querySelector(
-      `animation-panel[element-id="${this.elementId}"]`,
-    );
-    animationPanel.updateItem();
   }
 
   handleScroll(e) {

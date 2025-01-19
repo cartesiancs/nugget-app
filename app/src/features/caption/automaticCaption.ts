42405c76c7636098a9d77b2a354f390272b565d3
Huh Hyeongjun <hhj@devent.kr>
2025-01-19 21:25:34 +0900

    feat: preview auto caption

--- app/src/features/caption/automaticCaption.ts
+++ app/src/features/caption/automaticCaption.ts
@@ -19,6 +19,14 @@ export class AutomaticCaption extends LitElement {
   videoRows: any;
   hasUpdatedOnce: boolean;
   panelVideoModal: any;
+  isPlay: boolean;
+  private _start: any;
+  private _previousTimeStamp: any;
+  private _done: boolean;
+  private _animationFrameId: any;
+  progress: number;
+  previousProgress: number;
+
   constructor() {
     super();
 
@@ -33,6 +41,15 @@ export class AutomaticCaption extends LitElement {
     this.selectedRow = null;
 
     this.hasUpdatedOnce = false;
+
+    this.isPlay = false;
+    this.progress = 0;
+    this.previousProgress = 0;
+
+    this._start = undefined;
+    this._previousTimeStamp = undefined;
+    this._done = false;
+    this._animationFrameId = null;
 
     window.electronAPI.res.ffmpeg.extractAudioFromVideoProgress(
       (event, progress) => {
@@ -57,6 +74,8 @@ export class AutomaticCaption extends LitElement {
     );
   }
 
+  @query("#previewCanvasCaption") canvas!: HTMLCanvasElement;
+
   private lc = new LocaleController(this);
 
   createRenderRoot() {
@@ -73,8 +92,6 @@ export class AutomaticCaption extends LitElement {
     const serverUrl = document.querySelector("#NuggetAutoServer").value;
     const response = await fetch(audioPath);
 
-    console.log("AAAA");
-
     if (!response.ok) {
       throw new Error(`Failed to fetch audio file: ${response.statusText}`);
     }
@@ -84,7 +101,7 @@ export class AutomaticCaption extends LitElement {
     const formData = new FormData();
     formData.append("file", audioBlob, "audio.wav");
 
-    const request = await axios.post(`${serverUrl}/audio`, formData);
+    const request = await axios.post(`${serverUrl}/audio/test`, formData);
 
     const result = request.data.result;
 
@@ -184,13 +201,107 @@ export class AutomaticCaption extends LitElement {
     this.hasUpdatedOnce = true;
   }
 
+  _step(timestamp) {
+    if (this._start === undefined) {
+      this._start = timestamp;
+    }
+    const elapsed = timestamp - this._start;
+    this.progress = this.previousProgress + elapsed;
+
+    const video: HTMLVideoElement = document.querySelector(
+      "#captionPreviewVideo",
+    );
+
+    const ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(video, 0, 0, 1920, 1080);
+
+    this.requestUpdate();
+
+    if (!this._done) {
+      this._animationFrameId = window.requestAnimationFrame((ts) =>
+        this._step(ts),
+      );
+    }
+  }
+
+  playVideo() {
+    this.isPlay = true;
+    this._start = undefined;
+    this._previousTimeStamp = undefined;
+    this._done = false;
+
+    if (this._animationFrameId) {
+      window.cancelAnimationFrame(this._animationFrameId);
+    }
+
+    this._animationFrameId = window.requestAnimationFrame((ts) =>
+      this._step(ts),
+    );
+
+    const video: HTMLVideoElement = document.querySelector(
+      "#captionPreviewVideo",
+    );
+    video.play();
+
+    this.requestUpdate();
+  }
+
+  stopVideo() {
+    this.isPlay = false;
+    this.previousProgress = this.progress;
+    const video: HTMLVideoElement = document.querySelector(
+      "#captionPreviewVideo",
+    );
+    video.pause();
+
+    if (this._animationFrameId) {
+      window.cancelAnimationFrame(this._animationFrameId);
+    }
+    this._done = true;
+
+    this.requestUpdate();
+  }
+
+  resetVideo() {
+    this.isPlay = false;
+    this._done = true;
+    this.progress = 0;
+    this.previousProgress = 0;
+
+    const video: HTMLVideoElement = document.querySelector(
+      "#captionPreviewVideo",
+    );
+    video.currentTime = 0;
+
+    const ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(video, 0, 0, 1920, 1080);
+
+    this.requestUpdate();
+  }
+
   render() {
     let analyzedTextMap: any = [];
 
     for (let index = 0; index < this.analyzedText.length; index++) {
-      const element = this.analyzedText[index];
+      const element: any = this.analyzedText[index];
+      let partText: any = [];
+
+      for (let index = 0; index < element.words.length; index++) {
+        const partElement = element.words[index];
+        const isNow =
+          this.progress / 1000 > partElement.start &&
+          this.progress / 1000 < partElement.end;
+
+        partText.push(
+          html`<span class="${isNow ? "caption-part active" : "caption-part"}"
+            >${partElement.word}</span
+          >`,
+        );
+      }
       analyzedTextMap.push(
-        html`<span class="text-light caption">${element.text}</span>`,
+        html`<span class="text-light caption"
+          >${partText} - (${element.start}s -${element.end}s)</span
+        >`,
       );
     }
 
@@ -204,6 +315,32 @@ export class AutomaticCaption extends LitElement {
           border-radius: 8px;
           height: fit-content;
           width: fit-content;
+        }
+
+        .caption-part {
+          background-color: #1b1a1c;
+          color: #ffffff;
+          padding: 0.125rem 0.2rem;
+          margin-bottom: 0.1rem;
+          margin-left: 0.5rem;
+          border: 2px solid #26262b;
+          border-radius: 8px;
+          height: fit-content;
+          width: fit-content;
+          display: inline-block;
+        }
+
+        .caption-part.active {
+          background-color: #423d47;
+          color: #ffffff;
+          padding: 0.125rem 0.2rem;
+          margin-bottom: 0.1rem;
+          margin-left: 0.5rem;
+          border: 2px solid #3838d3;
+          border-radius: 8px;
+          height: fit-content;
+          width: fit-content;
+          display: inline-block;
         }
       </style>
       <div
@@ -345,11 +482,46 @@ export class AutomaticCaption extends LitElement {
           <div class="modal-content bg-dark">
             <div class="modal-body">
               <div class="d-flex col gap-2 mt-4">
-                <video
-                  width="200"
-                  class="${!this.isLoadVideo ? "d-none" : ""} col-3"
-                  src=${this.videoPath}
-                ></video>
+                <div class="col-3"></div>
+                <div
+                  class="d-flex row gap-2"
+                  style="width: 200px;position: fixed;"
+                >
+                  <canvas
+                    id="previewCanvasCaption"
+                    style="width: 200px;"
+                    width="1920"
+                    height="1080"
+                  ></canvas>
+
+                  <video
+                    class="d-none col-3"
+                    id="captionPreviewVideo"
+                    src=${this.videoPath}
+                  ></video>
+
+                  <span class="text-light"
+                    >${Math.round(this.progress / 1000)}s</span
+                  >
+
+                  <div class="d-flex col gap-2">
+                    <button @click=${this.resetVideo}>reset</button>
+                    <button
+                      class="${this.isPlay ? "d-none" : ""}"
+                      @click=${this.playVideo}
+                    >
+                      play
+                    </button>
+
+                    <button
+                      class="${!this.isPlay ? "d-none" : ""}"
+                      @click=${this.stopVideo}
+                    >
+                      stop
+                    </button>
+                  </div>
+                </div>
+
                 <div class="col-9">
                   <div class="d-flex row gap-2">${analyzedTextMap}</div>
                 </div>

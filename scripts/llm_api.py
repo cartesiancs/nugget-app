6d8729059caecedaf3a9b660e73fd47bd67dd77b
Abhinav <this.abhinav26@gmail.com>
2025-06-14 17:21:53 +0530

    comms

--- scripts/llm_api.py
+++ scripts/llm_api.py
@@ -9,8 +9,11 @@ router = FastAPI()
 @router.get("/api/llm")
 async def getResponseFromLlama3(request):
     try:
-        response = requests.get("http://192.168.46.138:3001/llm", data=request.data)
-        return response.json()
+        print("request for llm:", request)
+        return True
+        # response = requests.get("http://192.168.46.138:3001/llm", data=request.data)
+        # print("response from llm:", response)
+        # return response
     except requests.RequestException as e:
         raise HTTPException(
             status_code=500, detail=f"Error communicating with LLM service: {str(e)}"

8ec6cbcda13fa697ca2e0fd681dc0335c1fdabbe
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-06-15 02:53:49 +0530

    merger

--- apps/app/reponseHandlers/image_handlers.ts
+++ apps/app/reponseHandlers/image_handlers.ts
@@ -12,10 +12,10 @@ export function renderNewImage(absolute_path: string, superRes?: boolean): boole
   let timeline = timelineState.timeline;
   console.log(timeline);
   timeline[ide].localpath = absolute_path;
-  let oldwidth = timeline[ide].width
-  let oldheight = timeline[ide].height
-  timeline[ide].width = oldwidth * 4
-  timeline[ide].height = oldheight * 4
+  let oldwidth = timeline[ide].width;
+  let oldheight = timeline[ide].height;
+  timeline[ide].width = oldwidth * 4;
+  timeline[ide].height = oldheight * 4;
   timelineState.patchTimeline(timeline);
 
   previewCanvas.drawCanvas();

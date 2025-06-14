00b98c7aa91f74aeec4a65dfaad26dfa8910bb5e
Anshuman Sharma <69304992+and-human@users.noreply.github.com>
2025-06-14 14:11:53 +0530

    added routes for audio tools

--- scripts/audio_api.py
+++ scripts/audio_api.py
@@ -1,1 +1,13 @@
-# GAAND MASTI
+from main import router
+
+@router.get("/api/audio/remove-noise")
+def api_audio_remove_noise():
+    pass
+
+@router.get("/api/audio/text-to-speech")
+def api_audio_text_to_speech():
+    pass
+
+@router.get("/api/audio/transcribe")
+def api_audio_transcribe():
+    pass

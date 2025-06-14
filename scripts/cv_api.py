519470e0089f977e94488b3a104f441d159b2c47
Arnav Rustagi <arnav.rustagi@quizizz.com>
2025-06-14 13:50:54 +0530

    feat: added initial boilerplate for python3 server

---
+++ scripts/cv_api.py
@@ -0,0 +1,21 @@
+from main import router
+
+@router.get("/api/image/remove-bg")
+def api_image_background_removal():
+    pass 
+
+@router.get("api/image/color-grading")
+def api_image_color_grading():
+    pass
+
+@router.get("api/image/image-generation")
+def api_image_generate_image():
+    pass
+
+@router.get("api/image/portrait-effect")
+def api_image_portrait_effect():
+    pass
+
+@router.get("api/image/super-resolution")
+def api_image_super_resolution():
+    pass
